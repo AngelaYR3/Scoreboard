039dac6df1018adae45a8fa398ff9c4e581283b2
Angela <angelayourant@nycstudents.net>
2025-11-23 02:44:25 -0500

    test cases

--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
-
 public class Scoreboard {
+
+    private String teamOne;
+    private String teamTwo;
+    private int teamOneScore;
+    private int teamTwoScore;
+    private String activeTeam;
+
+    public Scoreboard(String one, String two) {
+        teamOne = one;
+        teamTwo = two;
+        activeTeam = teamOne;
+    }
+
+    public String getScore() {
+        return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+    }
+
+    public void recordPlay(int score) {
+        if (score == 0) {
+            if (activeTeam.equals(teamOne)) {
+                activeTeam = teamTwo;
+            }
+            else {
+                activeTeam = teamOne;
+            }
+        } else {
+            if (activeTeam.equals(teamOne))
+            {
+                teamOneScore += score;
+            } else {
+                teamTwoScore += score;
+            }
+        }
+    }
+
 }
